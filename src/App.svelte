1e3c486611e593b3e0b7e78e0f8de8c63b772896
Yerko Palma <yerko.palma@usach.cl>
2021-01-27 18:33:50 -0300

    agregado svelte-routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,26 @@
 <script>
-  import Banner from './ui/Banner.svelte'
-  import DirectLink from './ui/DirectLink.svelte'
+  import { Router, link, Route } from 'svelte-routing'
+  import Index from './pages/Index.svelte'
   import Footer from './ui/Footer.svelte'
 
-  let links = []
-  ;(async () => {
-    const response = await fetch(process.env.ACCESOS_DIRECTOS_URL)
-    links = await response.json()
-  })()
+  export let url = ''
 </script>
 
-<main class="my-0 mx-auto relative max-w-4xl bg-transparent p-8 box-border z-10 pb-16 sm:pt-16">
-  <Banner></Banner>
-  <div class="flex-wrap flex items-center justify-evenly">
-    {#each links as {Imagen, Titulo, Link}}
-      <DirectLink chico width="40%" src={Imagen.url} href={Link} desc={Titulo} ></DirectLink>
-    {/each}
+<Router {url}>
+  <div class="flex justify-between px-8 items-center py-6 md:justify-start md:space-x-10">
+    <nav class="hidden md:flex space-x-10">
+      <a class="text-base font-medium text-gray-500 hover:text-gray-900" href="/" use:link>Inicio</a>
+      <a class="text-base font-medium text-gray-500 hover:text-gray-900" href="/resources" use:link>Recursos</a>
+    </nav>
   </div>
-</main>
-<Footer></Footer>
 
+  <main class="my-0 mx-auto relative max-w-4xl bg-transparent p-8 box-border z-10 pb-16 sm:pt-16">
+    <Route path='/' component={Index}></Route>
+    <Route path='/resources'>Recursos...</Route>
+  </main>
+</Router>
+
+<Footer></Footer>
 
 <svelte:head>
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
---
+++ src/pages/Index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Banner from '../ui/Banner.svelte'
+  import DirectLink from '../ui/DirectLink.svelte'
+
+  let links = []
+  ;(async () => {
+    const response = await fetch(process.env.ACCESOS_DIRECTOS_URL)
+    links = await response.json()
+  })()
+</script>
+<Banner></Banner>
+<div class="flex-wrap flex items-center justify-evenly">
+  {#each links as {Imagen, Titulo, Link}}
+    <DirectLink chico width="40%" src={Imagen.url} href={Link} desc={Titulo} ></DirectLink>
+  {/each}
+</div>
