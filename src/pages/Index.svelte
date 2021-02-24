b4c322e6ebcf35fcf5e7d8a564a4c22e70f1729c
Yerko Palma <yerko.palma@usach.cl>
2021-02-24 20:25:09 -0300

    agrega componente de cargando

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -1,16 +1,26 @@
 <script>
   import Banner from '../ui/Banner.svelte'
+  import Loading from '../ui/Loading.svelte'
   import DirectLink from '../ui/DirectLink.svelte'
 
   let links = []
+  let loading = true
   ;(async () => {
     const response = await fetch(process.env.ACCESOS_DIRECTOS_URL)
     links = await response.json()
+    if (response.ok) {
+      // loading = false
+    }
   })()
 </script>
-<Banner></Banner>
-<div class="flex-wrap flex items-center justify-evenly">
-  {#each links as {Imagen, Titulo, Link}}
-    <DirectLink chico width="40%" src={Imagen.url} href={Link} desc={Titulo} ></DirectLink>
-  {/each}
-</div>
+
+{#if loading}
+  <Loading></Loading>
+{:else}
+  <Banner></Banner>
+  <div class="flex-wrap flex items-center justify-evenly">
+    {#each links as {Imagen, Titulo, Link}}
+      <DirectLink chico width="40%" src={Imagen.url} href={Link} desc={Titulo} ></DirectLink>
+    {/each}
+  </div>
+{/if}
